ca034bbefcd0ddec7e0aafbb43a2839325294468
Maria Luisa <araujocostamalu@gmail.com>
2023-07-18 09:48:42 -0300

    chore: setup project

---
+++ __tests__/handler.test.ts
@@ -0,0 +1,20 @@
+import { handler } from '../src/index';
+
+describe('handler', () => {
+    test('should return a valid response', async () => {
+        const expectedResponse = {
+            statusCode: 200,
+            body: JSON.stringify(
+                {
+                    message: "Go Serverless v3.0! Your function executed successfully!",
+                },
+                null,
+                2
+            ),
+        };
+
+        const response = await handler();
+
+        expect(response).toEqual(expectedResponse);
+    });
+});
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export const handler = async (): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+      },
+      null,
+      2
+    ),
+  };
+};
